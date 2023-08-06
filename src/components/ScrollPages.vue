25beaf4f821a62590096e596a4b51a2832d8663f
DolorGloria <imwangqp@outlook.com>
2023-08-06 08:44:48 +0800

    Commit on 0806

---
+++ src/components/ScrollPages.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "ScrollPages"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/components/chart/UnitVisualization.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "UnitVisualization"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="less">
+
+</style>
